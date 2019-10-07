a471a5b02e7690f81bf5499c9b722d616643e02b
Jason Osao <josako89@gmail.com>
2019-10-06 19:39:34 -0700

    modified index.html and added questions to js

---
+++ questions.js
@@ -0,0 +1,44 @@
+var questions = [
+    {
+        title: "Inside which HTML element do put the Javascript?",
+        choices: ["<iscipt>", "<script>", "<javascript>", "<js>"],
+        answer: "<script>"
+    },
+    {
+        title: "What is the correct syntax to start a FOR loop?",
+        choices: ["for(i-1=0)", "for(i=55-1;54)", "for(i=0;i<=4;i++", "all of the above"],
+        answer: "for(i=0;i<=4;i++"
+    },
+    {
+        title: "What is the proper way to add a comment in JavaScript that will not affect the code?",
+        choices: [
+            "you are unable to make comments in JavaScript",
+            "'this is how you comment'",
+            "//this is how you comment",
+            "<this is how you insert a comment>"
+        ],
+        answer: "//this is how you comment"
+    },
+    {
+        title:
+            "What is JavaScript?",
+        choices: [
+            "An object-oriented computer programming language used to create interactive effects within web browsers",
+            "The standard markup language for documents designed to be displayed in a web browser",
+            "A stylesheet language used to describe the presentaiton of a document written in HTML",
+            "none of the above",
+        ],
+        answer: "An object-oriented computer programming language used to create interactive effects within web browsers"
+    },
+    {
+        title:
+            "Correct syntax to create a function in JavaScript:",
+        choices: [
+            "function = create.Function()",
+            "function createFunction()",
+            "function = createFunction()",
+            "function === createFunction()",
+        ],
+        answer: "function createFunction()"
+    }
+];
